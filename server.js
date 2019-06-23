251c2cbfbde810b680b595e6a08f26277d908e60
Piotr Dzienis <dojlidy.zubr@gmail.com>
2019-06-23 18:15:47 +0200

    task

--- server.js
+++ server.js
@@ -1,18 +1,17 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static('assets'));
-
-app.get('/', function (req, res) {
-    res.sendFile('/index.html');
+app.use(function(req, res, next){
+    console.log('Jestem pośrednikiem przy żądaniu do /store');
+    next();
 });
 
-app.get('/userform', function (req, res) {
-    const response = {
-        first_name: req.query.first_name,
-        last_name: req.query.last_name
-    };
-    res.json(response);
+app.get('/store', function (req, res) {
+    res.send('To jest sklep');
+});
+
+app.get('/', function(req, res) {
+    res.send('Hello World');
 });
 
 var server = app.listen(3000, 'localhost', function() {
@@ -23,5 +22,5 @@ var server = app.listen(3000, 'localhost', function() {
 });
 
 
-app.listen(3000);
+
 
